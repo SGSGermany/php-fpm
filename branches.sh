9ab59f314fa3462195566e3fbece6f66cd68529f
Daniel Rudolf <github.com@daniel-rudolf.de>
2026-02-13 17:58:42 +0100

    Add `./branches.sh` to ensure building the latest PHP branch
    `./branches.sh` will print a warning when e.g. PHP 9.0 is released, but no build instructions were added to the `./branches/` folder yet. We will attempt to build that milestone using our defaults, but if the Docker image isn't up-to-date yet either, the build will fail. This is intentional, we want that heads-up. Other PHP milestones should build just fine though.

---
+++ branches.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# php-fpm
+# A php-fpm container with an improved configuration.
+#
+# Copyright (c) 2021  SGS Serious Gaming & Simulations GmbH
+#
+# This work is licensed under the terms of the MIT license.
+# For a copy, see LICENSE file or <https://opensource.org/licenses/MIT>.
+#
+# SPDX-License-Identifier: MIT
+# License-Filename: LICENSE
+
+set -eu -o pipefail
+export LC_ALL=C.UTF-8
+
+[ -v CI_TOOLS ] && [ "$CI_TOOLS" == "SGSGermany" ] \
+    || { echo "Invalid build environment: Environment variable 'CI_TOOLS' not set or invalid" >&2; exit 1; }
+
+[ -v CI_TOOLS_PATH ] && [ -d "$CI_TOOLS_PATH" ] \
+    || { echo "Invalid build environment: Environment variable 'CI_TOOLS_PATH' not set or invalid" >&2; exit 1; }
+
+source "$CI_TOOLS_PATH/helper/common.sh.inc"
+
+BUILD_DIR="$(CDPATH= cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
+
+VERSION_URL="https://www.php.net/releases/index.php?json"
+
+echo + "BRANCHES_LOCAL=\"\$(find ./branches/ -mindepth 1 -maxdepth 1 -type d -printf '%f\n')\"" >&2
+BRANCHES_LOCAL="$(find "$BUILD_DIR/branches/" -mindepth 1 -maxdepth 1 -type d -printf '%f\n')"
+
+echo + "BRANCHES_GLOBAL_JSON=\"\$(_curl_json $(quote "$VERSION_URL"))\"" >&2
+BRANCHES_GLOBAL_JSON="$(_curl_json "$VERSION_URL")"
+
+echo + "BRANCHES_GLOBAL=\"\$(jq -re '.[].supported_versions[]' <<< \"\$BRANCHES_GLOBAL_JSON\")\"" >&2
+BRANCHES_GLOBAL="$(jq -re '.[].supported_versions[]' <<< "$BRANCHES_GLOBAL_JSON")"
+
+echo + "BRANCHES_MISSING=\"\$(comm -13 <(sort <<< \"\$BRANCHES_LOCAL\") <(sort <<< \"\$BRANCHES_GLOBAL\"))\"" >&2
+BRANCHES_MISSING="$(comm -13 <(sort <<< "$BRANCHES_LOCAL") <(sort <<< "$BRANCHES_GLOBAL"))"
+
+if [ -n "$BRANCHES_MISSING" ]; then
+    echo "Explicit build instructions for the following PHP branches are missing, using defaults..." >&2
+    sed -e 's/^/- /' <<< "$BRANCHES_MISSING" >&2
+fi
+
+echo + "{ echo \"\$BRANCHES_LOCAL\"; echo \"\$BRANCHES_MISSING\"; } | sort_semver" >&2
+{ [ -z "$BRANCHES_LOCAL" ] || echo "$BRANCHES_LOCAL"; [ -z "$BRANCHES_MISSING" ] || echo "$BRANCHES_MISSING"; } \
+    | sort_semver
